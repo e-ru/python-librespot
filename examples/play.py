1b76624ad5617f1ff97f4903eb692861eca45019
e-ru <e.rudisch@gmx.de>
2021-02-08 14:59:36 +0100

    add sound output, remove device

--- examples/play.py
+++ examples/play.py
@@ -7,20 +7,13 @@ if len(sys.argv) != 4:
   print("Usage: %s USERNAME PASSWORD TRACK" % sys.argv[0])
   sys.exit(1)
 
-totalLen = 0
-class Sink:
-  def write(self, buf):
-    global totalLen
-    #print("%s" % repr(buf), end='', flush=True)
-    print('.', end='', flush=True)
-    totalLen += len(buf)
 
 username = sys.argv[1]
 password = sys.argv[2]
 trackid = SpotifyId(sys.argv[3])
 
 print("Connecting ...")
-session = Session.connect(username, password, Sink()).result()
+session = Session.connect(username, password).result()
 player = session.player()
 
 print("Playing ...")
@@ -32,4 +25,4 @@ print("Stop")
 player.stop()
 print(handle.result())
 
-print("Done", totalLen)
+print("Done")
